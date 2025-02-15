e1e9ea38506c0cb2f1aad277fc4869280719b561
Brian Richardson <brisquatch@gmail.com>
2025-02-15 09:05:15 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,249 @@
+<?php include('includes/head.php');?>
+  <?php include('includes/header.php');?>
+
+
+    <div id="wrapper">
+
+
+      <div class="portOptions">
+        <h1>portfolio</h1>
+        <!-- <h2><a href="#web">web</a></h2>
+                <p>&#124;</p>
+                <h2>design</h2> -->
+      </div>
+    </div>
+    <div class="intro">
+      <p class="introParagraph">I&rsquo;m Brian Richardson. Thanks for checking out my work. My background is originally in Studio Art. I like taking that same approach to web design. Sketch it out, consider relevant factors, pick great colors and textures, then&mdash; Boom. Start creating.</p>
+    </div>
+
+
+    <section class="works">
+
+      <div class="description">
+        <h3>Red Rock Behavioral Health Services</h3>
+        <h4>Design/Development</h4>
+        <h5><a href="http://www.red-rock.com" target="_blank">view live site</a></h5>
+
+        <p>Client website for Red Rock Behavioral Health Services, a multi-location chain in Oklahoma. The client wanted to re-design their site to match all new branding/marketing materials. HTML, CSS, PHP and JavaScript.</p>
+      </div>
+
+      <div id="screenshots">
+
+        <img class="modal-content desktop" id="myImg1" src="images/screenshots/desktop-assets/rrDesktop.png" alt="Red Rock Behavioral Health Services desktop screenshot">
+
+        <!-- The Modal -->
+        <div id="myModal1" class="modal">
+
+          <!-- The Close Button -->
+          <span class="close1" onclick="document.getElementById('myModal').style.display='none'">&times;</span>
+
+          <!-- Modal Content (The Image) -->
+          <img src="images/screenshots/desktop-assets/rrDesktop.png" alt="Red Rock Behavioral Health Services modal view for desktop" id="img01" class="modal-content">
+
+        </div>
+
+        <img class="modal-content iPad" id="myImg2" src="images/screenshots/iPad-assets/rrIpad.png" alt="red rock behavioral health services ipad screenshot">
+
+        <!-- The Modal -->
+        <div id="myModal2" class="modal">
+
+          <!-- The Close Button -->
+          <span class="close2" onclick="document.getElementById('myModal').style.display='none'">&times;</span>
+          <!-- Modal Content (The Image) -->
+          <img src="images/screenshots/iPad-assets/rrIpad.png" alt="Red Rock Behavioral Health Services modal view for ipad" id="img02" class="modal-content">
+
+        </div>
+
+        <img class="modal-content iPhone" id="myImg3" src="images/screenshots/iphone-assets/rrIphone.png" alt="red rock behavioral health services iphone screenshot">
+
+        <!-- The Modal -->
+        <div id="myModal3" class="modal">
+
+          <!-- The Close Button -->
+          <span class="close3" onclick="document.getElementById('myModal').style.display='none'">&times;</span>
+          <!-- Modal Content (The Image) -->
+          <img src="images/screenshots/iphone-assets/rrIphone.png" alt="Red Rock Behavioral Health Services modal view for iphone" id="img03" class="modal-content">
+
+
+        </div>
+
+        <p class="github">
+          <a href="https://github.com/BRichardson2993/red-rock" target="_blank"><img src="images/GitHub-Logos/GitHubSM.png" alt="view this repository on github"> repository</a>
+        </p>
+
+      </div>
+
+    </section>
+
+    <section class="works">
+
+      <div class="description">
+        <h3>Hayes Lawn and Landscape, LLC</h3>
+        <h4>Design/Development</h4>
+        <h5><a href="http://www.hayeslawnandlandscape.co" target="_blank">view live site</a></h5>
+        <p>This client is a rapidly growing Oklahoma landscape company. They desired a re&dash;design of their current website based on their newly re&dash;designed logo, vehicle designs, and marketing materials. The customer wanted to have the ability to make updates to images in the carousel based on new installations for landscaping and holiday lighting. They also wanted to be able to have a blog, job application forms, and contact forms for the customer to request quotes. The solution involved coding an original WordPress theme using HTML, CSS, and PHP. Once launched, I installed relevant plug-ins for security and SEO, and provided the customer training for them to maintain the site.</p>
+      </div>
+
+      <div id="screenshots">
+
+        <img class="modal-content desktop" id="myImg4" src="images/screenshots/desktop-assets/hayesDesktop.png" alt="hayes lawn and landscape okc desktop screenshot">
+
+        <!-- The Modal -->
+        <div id="myModal4" class="modal">
+
+          <!-- The Close Button -->
+          <span class="close4" onclick="document.getElementById('myModal').style.display='none'">&times;</span>
+
+          <!-- Modal Content (The Image) -->
+          <img src="images/screenshots/desktop-assets/rrDesktop.png" alt="hayes lawn and landscape okc modal view for desktop" id="img04" class="modal-content">
+
+        </div>
+
+        <img class="modal-content iPad" id="myImg5" src="images/screenshots/iPad-assets/hayesIpad.png" alt="hayes lawn and landscape okc ipad screenshot">
+
+        <!-- The Modal -->
+        <div id="myModal5" class="modal">
+
+          <!-- The Close Button -->
+          <span class="close5" onclick="document.getElementById('myModal').style.display='none'">&times;</span>
+          <!-- Modal Content (The Image) -->
+          <img src="images/screenshots/iPad-assets/hayesIpad.png" alt="hayes lawn and landscape okc modal view for ipad" id="img05" class="modal-content">
+
+        </div>
+
+
+        <img class="modal-content iPhone" id="myImg6" src="images/screenshots/iphone-assets/hayesIphone.png" alt="hayes lawn and landscape okc iphone screenshot">
+
+        <!-- The Modal -->
+        <div id="myModal6" class="modal">
+
+          <!-- The Close Button -->
+          <span class="close6" onclick="document.getElementById('myModal').style.display='none'">&times;</span>
+          <!-- Modal Content (The Image) -->
+          <img src="images/screenshots/iphone-assets/hayesIphone.png" alt="hayes lawn and landscape okc modal view for iphone" id="img06" class="modal-content">
+
+
+        </div>
+
+        <p class="github">
+          <a href="https://github.com/BRichardson2993/hayes" target="_blank"><img src="images/GitHub-Logos/GitHubSM.png" alt="view this repository on github"> repository</a>
+        </p>
+
+      </div>
+    </section>
+
+    <section class="works">
+
+      <div class="description">
+        <h3>Well Preserved Advisory Group</h3>
+        <h4>Development</h4>
+        <h5><a href="http://www.wellpreserved.info" target="_blank">view live site</a></h5>
+        <p>Eunice Khoury is host of Well Preserved Radio Show on KTOK 1000AM each Saturday at 7:00 am. The site was designed to accompany the radio show as well as contain the large amount of resources that Well Preserved offers for senior living. The target audience for this site is seniors, age 65+. They wanted to keep the site&rsquo;s usability as simple as possible. The mobile navigation menu was to display larger areas for links and not collapse so as to be easily viewable. The primary focus on this site was desktop view, as they felt fewer of their audience would be using mobile devices to view the site.</p>
+      </div>
+
+      <div id="screenshots">
+
+        <img class="modal-content desktop" id="myImg7" src="images/screenshots/desktop-assets/wpDesktop.png" alt="well preserved advisory group desktop screenshot">
+
+        <!-- The Modal -->
+        <div id="myModal7" class="modal">
+
+          <!-- The Close Button -->
+          <span class="close7" onclick="document.getElementById('myModal').style.display='none'">&times;</span>
+
+          <!-- Modal Content (The Image) -->
+          <img src="images/screenshots/desktop-assets/wpDesktop.png" alt="well preserved advisory group modal view for desktop" id="img07" class="modal-content">
+
+        </div>
+
+        <img class="modal-content iPad" id="myImg8" src="images/screenshots/iPad-assets/wpIpad.png" alt="well preserved advisory group ipad screenshot">
+
+        <!-- The Modal -->
+        <div id="myModal8" class="modal">
+
+          <!-- The Close Button -->
+          <span class="close8" onclick="document.getElementById('myModal').style.display='none'">&times;</span>
+          <!-- Modal Content (The Image) -->
+          <img src="images/screenshots/iPad-assets/wpIpad.png" alt="well preserved advisory group modal view for ipad" id="img08" class="modal-content">
+
+        </div>
+
+
+        <img class="modal-content iPhone" id="myImg9" src="images/screenshots/iphone-assets/wpIphone.png" alt="well preserved advisory group iphone screenshot">
+        <!-- The Modal -->
+        <div id="myModal9" class="modal">
+
+          <!-- The Close Button -->
+          <span class="close9" onclick="document.getElementById('myModal').style.display='none'">&times;</span>
+          <!-- Modal Content (The Image) -->
+          <img src="images/screenshots/iphone-assets/wpIphone.png" alt="well preserved advisory group modal view for iphone" id="img09" class="modal-content">
+
+
+        </div>
+
+        <p class="github">
+          <a href="https://github.com/BRichardson2993/well-preserved" target="_blank"><img src="images/GitHub-Logos/GitHubSM.png" alt="view this repository on github"> repository</a>
+        </p>
+
+      </div>
+    </section>
+
+    <section class="works">
+
+      <div class="description">
+        <h3>D.O.N.T.&mdash; Drivers Opposed to Neglecting Toddlers</h3>
+        <h4>BPA National Competition&mdash; <span class="winning">WINNING WEBSITE</span></h4>
+        <h5><a href="http://www.websquatch.com/dont" target="_blank">view live site</a></h5>
+        <p>This was a team project for Francis Tuttle Web Design &amp; Development. The assignment was to create a PSA website. We initially won the state competition, allowing us to compete at the national level in Boston, MA. We were able to defend our school's national title as we became 2016 national champions.</p>
+      </div>
+
+      <div id="screenshots">
+
+        <img class="modal-content desktop" id="myImg10" src="images/screenshots/desktop-assets/dontDesktop.png" alt="drivers opposed to neglecting toddlers desktop screenshot">
+
+        <!-- The Modal -->
+        <div id="myModal10" class="modal">
+
+          <!-- The Close Button -->
+          <span class="close10" onclick="document.getElementById('myModal').style.display='none'">&times;</span>
+
+          <!-- Modal Content (The Image) -->
+          <img src="images/screenshots/desktop-assets/dontDesktop.png" alt="drivers opposed to neglecting toddlers modal view for desktop" id="img10" class="modal-content">
+
+        </div>
+
+        <img class="modal-content iPad" id="myImg11" src="images/screenshots/iPad-assets/dontIpad.png" alt="drivers opposed to neglecting toddlers ipad screenshot">
+
+        <!-- The Modal -->
+        <div id="myModal11" class="modal">
+
+          <!-- The Close Button -->
+          <span class="close11" onclick="document.getElementById('myModal').style.display='none'">&times;</span>
+          <!-- Modal Content (The Image) -->
+          <img src="images/screenshots/iPad-assets/dontIpad.png" alt="drivers opposed to neglecting toddlers modal view for ipad" id="img11" class="modal-content">
+
+        </div>
+
+
+        <img class="modal-content iPhone" id="myImg12" src="images/screenshots/iphone-assets/dontIphone.png" alt="drivers opposed to neglecting toddlers iphone screenshot">
+        <!-- The Modal -->
+        <div id="myModal12" class="modal">
+
+          <!-- The Close Button -->
+          <span class="close12" onclick="document.getElementById('myModal').style.display='none'">&times;</span>
+          <!-- Modal Content (The Image) -->
+          <img src="images/screenshots/iphone-assets/dontIphone.png" alt="drivers opposed to neglecting toddlers modal view for iphone" id="img12" class="modal-content">
+
+
+        </div>
+
+        <p class="github">
+          <a href="https://github.com/BRichardson2993/lucky-crocodile" target="_blank"><img src="images/GitHub-Logos/GitHubSM.png" alt="view this repository on github"> repository</a>
+        </p>
+
+
+      </div>
+    </section>
+
+    <span class="top"><a href="#logo">back to top</a></span>
+    <?php include('includes/footer.php');?>
